723116d24ae16773b359641f223e1a729ea1c65c
AliakbarSu <aliakbar.su@hotmail.com>
2021-05-16 23:14:23 +1200

    initial commit

---
+++ src/data/config.js
@@ -0,0 +1,34 @@
+module.exports = {
+  defaultTitle: 'Ali Sultani',
+  logo: 'https://portfolio.smakosh.com/favicon/favicon-512.png',
+  author: 'John Doe',
+  url: 'http://alisultani.com',
+  legalName: 'John Doe',
+  defaultDescription: 'I’m John and I’m a Backend & Devops engineer!',
+  socialLinks: {
+    twitter: 'http://www.twitter.com',
+    github: 'https://github.com',
+    linkedin: 'https://www.linkedin.com/in/ismail-ghallou-630149122/',
+    instagram: 'https://instagram.com/',
+    youtube: 'https://www.youtube.com',
+    google: 'https://plus.google.com',
+  },
+  googleAnalyticsID: 'UA-88875900-4',
+  themeColor: '#6b63ff',
+  backgroundColor: '#6b63ff',
+  social: {
+    facebook: 'appId',
+    twitter: 'to-be',
+  },
+  address: {
+    city: 'City',
+    region: 'Region',
+    country: 'Country',
+    zipCode: 'ZipCode',
+  },
+  contact: {
+    email: 'email',
+    phone: 'phone number',
+  },
+  foundingDate: '2018',
+};
