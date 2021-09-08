f01865c05d64d76648a053ddc043cb7f09a2bdd7
AliakbarSu <aliakbar.su@hotmail.com>
2021-09-08 15:45:48 +1200

    Update profile graphql

--- src/components/landing/Projects/index.jsx
+++ src/components/landing/Projects/index.jsx
@@ -1,40 +1,42 @@
-import React, { useContext } from 'react';
-import { useStaticQuery, graphql } from 'gatsby';
-import { ThemeContext } from 'providers/ThemeProvider';
-import { Container, Card, TitleWrap } from 'components/common';
-import Star from 'components/common/Icons/Star';
-import Fork from 'components/common/Icons/Fork';
-import { Wrapper, Grid, Item, Content, Stats, Languages } from './styles';
+import React, { useContext } from 'react'
+import { useStaticQuery, graphql } from 'gatsby'
+import { ThemeContext } from 'providers/ThemeProvider'
+import { Container, Card, TitleWrap } from 'components/common'
+import Star from 'components/common/Icons/Star'
+import Fork from 'components/common/Icons/Fork'
+import { Wrapper, Grid, Item, Content, Stats, Languages } from './styles'
 
 // export const Projects = () => <Wrapper>Test</Wrapper>;
 export const Projects = () => {
-  const { theme } = useContext(ThemeContext);
+  const { theme } = useContext(ThemeContext)
   const {
     github: {
       viewer: {
-        repositories: { edges },
-      },
-    },
+        pinnedItems: { edges }
+      }
+    }
   } = useStaticQuery(
     graphql`
       {
         github {
           viewer {
-            repositories(first: 6, orderBy: { field: STARGAZERS, direction: DESC }) {
+            pinnedItems(first: 10) {
               edges {
                 node {
-                  id
-                  name
-                  url
-                  description
-                  stargazers {
-                    totalCount
-                  }
-                  forkCount
-                  languages(first: 3) {
-                    nodes {
-                      id
-                      name
+                  ... on GitHub_Repository {
+                    id
+                    name
+                    url
+                    description
+                    stargazers {
+                      totalCount
+                    }
+                    forkCount
+                    languages(first: 3) {
+                      nodes {
+                        id
+                        name
+                      }
                     }
                   }
                 }
@@ -44,13 +46,20 @@ export const Projects = () => {
         }
       }
     `
-  );
+  )
   return (
     <Wrapper as={Container} id="projects">
       <h2>Projects</h2>
       <Grid>
         {edges.map(({ node }) => (
-          <Item key={node.id} as="a" href={node.url} target="_blank" rel="noopener noreferrer" theme={theme}>
+          <Item
+            key={node.id}
+            as="a"
+            href={node.url}
+            target="_blank"
+            rel="noopener noreferrer"
+            theme={theme}
+          >
             <Card theme={theme}>
               <Content>
                 <h4>{node.name}</h4>
@@ -80,5 +89,5 @@ export const Projects = () => {
         ))}
       </Grid>
     </Wrapper>
-  );
-};
+  )
+}
