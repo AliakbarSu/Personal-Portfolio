76d58e597810b9172c89e3b4eaa44e155e3bd11c
AliakbarSu <aliakbar.su@hotmail.com>
2021-10-05 14:05:38 +1300

    Remove window from header

--- src/components/theme/Header/NavbarLinks/index.jsx
+++ src/components/theme/Header/NavbarLinks/index.jsx
@@ -7,13 +7,14 @@ import { Wrapper } from './styles'
 const NavbarLinks = ({ desktop }) => {
   const { theme } = useContext(ThemeContext)
 
-  const isHome = window.location.pathname === '/'
+  // const isHome = window.location.pathname === '/'
 
   return (
     <Wrapper desktop={desktop} theme={theme}>
       <a href="/about">About</a>
-      {isHome && <AnchorLink href="#posts">Posts</AnchorLink>}
-      {!isHome && <a href="/#posts">Posts</a>}
+      {/* {isHome && <AnchorLink href="#posts">Posts</AnchorLink>} */}
+      {/* {!isHome && <a href="/#posts">Posts</a>} */}
+      <a href="/#posts">Posts</a>
       <ToggleTheme />
     </Wrapper>
   )
