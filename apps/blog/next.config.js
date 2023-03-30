53123f5deee796b0d7daeefbd88d48104605a80f
Ali Sultani <aliakbar.su@hotmail.com>
2023-03-30 15:17:56 +1300

    Add tracingIncludes

--- apps/blog/next.config.js
+++ apps/blog/next.config.js
@@ -6,6 +6,9 @@ const nextConfig = {
   output: 'standalone',
   experimental: {
     outputFileTracingRoot: path.join(__dirname, '../../'),
+    outputFileTracingIncludes: {
+      '*': ['./node_modules/next/**/*'],
+    }
   },
   typescript: {
     ignoreBuildErrors: true
