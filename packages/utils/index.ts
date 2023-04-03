163c8f31bd9407597ade43f81507a3caa226ba2a
Ali Sultani <aliakbar.su@hotmail.com>
2023-04-04 10:24:08 +1200

    Update mail service to use different auth system

--- packages/utils/index.ts
+++ packages/utils/index.ts
@@ -1,26 +1,28 @@
-import nodeMailer from 'nodemailer';
+import nodeMailer from 'nodemailer'
 
-
-const TO_EMAIL = "hello@alisultani.com";
-export const sendEmail = async (name: string, from: string, messsage: string, application: "Bog" | "Portfolio") => {
-    const account = {
-        user: process.env.GMAIL_USER || "",
-        pass: process.env.GMAIL_PASS || "",
+const TO_EMAIL = 'hello@alisultani.com'
+export const sendEmail = async (
+  name: string,
+  from: string,
+  messsage: string,
+  application: 'Bog' | 'Portfolio'
+) => {
+  const transporter = nodeMailer.createTransport({
+    host: 'smtp.gmail.com',
+    port: 465,
+    secure: true,
+    auth: {
+      type: 'OAuth2',
+      serviceClient: process.env.GMAIL_SERVICE_CLIENT,
+      privateKey: process.env.GMAIL_PRIVATE_KEY
     }
-    
-    const transporter = nodeMailer.createTransport("SMTP", {
-        service: "gmail",
-        auth: {
-        user: account.user,
-        pass: account.pass,
-        },
-    });
-    
-    const info = await transporter.sendMail({
-        from: from,
-        to: TO_EMAIL,
-        subject: `Contact Form: (${name})}) - ${application}`,
-        text: messsage,
-    })
-    return info
+  })
+
+  const info = await transporter.sendMail({
+    from: from,
+    to: TO_EMAIL,
+    subject: `Contact Form: (${name})}) - ${application}`,
+    text: messsage
+  })
+  return info
 }
