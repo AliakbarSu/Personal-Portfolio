d554840d108f02107bf0e075b57df03280ac215c
Ali Sultani <aliakbar.su@hotmail.com>
2023-07-27 13:08:35 +1200

    Add link for Hire Me button

--- apps/portfolio/src/components/landing/Skills/index.tsx
+++ apps/portfolio/src/components/landing/Skills/index.tsx
@@ -1,30 +1,34 @@
-import React, { useContext } from 'react';
-import AnchorLink from 'react-anchor-link-smooth-scroll';
-import { ThemeContext } from 'ui/providers/ThemeProvider';
-import { Container, Button } from '@/components/common';
-import { Wrapper, SkillsWrapper, Details, Thumbnail } from './styles';
+import React, { useContext } from 'react'
+import { ThemeContext } from 'ui/providers/ThemeProvider'
+import { Container, Button } from '@/components/common'
+import { Wrapper, SkillsWrapper, Details, Thumbnail } from './styles'
+import Link from 'next/link'
 
 export const Skills = () => {
-  const { theme } = useContext(ThemeContext);
+  const { theme } = useContext(ThemeContext)
 
   return (
     <Wrapper id="about">
       <SkillsWrapper as={Container}>
         <Thumbnail>
-          <img src="/assets/illustrations/skills.svg" alt="I’m Ali and I’m a Full Stack developer!" />
+          <img
+            src="/assets/illustrations/skills.svg"
+            alt="I’m Ali and I’m a Full Stack developer!"
+          />
         </Thumbnail>
         <Details theme={theme}>
           <h1>More about me</h1>
           <p>
-            I'm a self-taught web developer whose passion is to build new things everyday. Beside my huge passion for
-            web development i'm also in love with electronics.
+            I'm a self-taught web developer whose passion is to build new things
+            everyday. Beside my huge passion for web development i'm also in
+            love with electronics.
           </p>
           {/* <Button as={AnchorLink} href="#contact"> */}
-          <Button>
-            Hire me
-          </Button>
+          <Link href="/#contact">
+            <Button>Hire me</Button>
+          </Link>
         </Details>
       </SkillsWrapper>
     </Wrapper>
-  );
-};
+  )
+}
